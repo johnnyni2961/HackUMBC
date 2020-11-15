34997f9e5056870aa0bd7905f5441d9fa12701da
Lynne Cure <pjh.cure@gmail.com>
2020-11-14 20:43:01 -0500

    Update

--- hackumbc/src/routes.js
+++ hackumbc/src/routes.js
@@ -2,6 +2,11 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from './components/Home'
 import LoginComponent from './components/LoginComponent'
+import tutor from './components/tutor'
+import register from './components/register'
+import subject from './components/subject'
+import tutorsignin from './components/tutor-signin'
+import tutorsignup from './components/tutor-signup'
 
 Vue.use(VueRouter);
 
@@ -16,6 +21,31 @@ const routes = [
         path:'/login',
         name: "login",
         component: LoginComponent
+    },
+    {    
+        path: '/tutor',
+        name: "tutor",
+        component: tutor
+    },
+    {
+        path: '/register',
+        name: "register",
+        component: register
+    },
+    {
+        path: '/subject',
+        name: "subject",
+        component: subject
+    },
+    {
+        path: '/tutor-signin',
+        name: "tutorsignin",
+        component: tutorsignin
+    },
+    {
+        path: '/tutor-signup',
+        name: "tutorsignup",
+        component: tutorsignup
     }
 ]
 
